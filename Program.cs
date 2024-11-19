29580fb82c639d0e85a03d5e2b27fd8282dc7fa4
sujit shrestha <sujitshrestha2060@gamil.com>
2024-11-19 18:04:52 +0545

    swap variable value wihout the using another variable

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int var1 = 10;
+            int var2 = 20;
+            Console.WriteLine($"Original var1: {var1}");
+            Console.WriteLine($"Original var2: {var2}");
+            var1 = var1 + var2;  
+            var2 = var1 - var2; 
+            var1 = var1 - var2;
+            Console.WriteLine($"Swapped var1: {var1}");
+            Console.WriteLine($"Swapped var2: {var2}");
+        }
+                
+        
+    }
+}
