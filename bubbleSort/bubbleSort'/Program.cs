6ceffd0238bc992343de6f4ffc7fbb9184bc1294
sujit shrestha <sujitshrestha2060@gamil.com>
2024-12-06 19:47:19 +0545

    sort array using the bubble sort

---
+++ bubbleSort/bubbleSort'/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BubbleSort
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] array = { 7,6,9,2,3,6,1,4,5 };
+            SortAlgo sortAlgo = new SortAlgo();
+
+            Console.WriteLine("Before Sorting:");
+            sortAlgo.displayArray(array);
+
+            sortAlgo.bubbleSort(array);
+
+            Console.WriteLine("After Sorting:");
+            sortAlgo.displayArray(array);
+        }
+    }
+}
---
+++ bubbleSort/bubbleSort'/SortAlgo.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BubbleSort
+{
+    internal class SortAlgo
+    {
+        public void bubbleSort(int[] arr)
+        {
+            int n = arr.Length;
+            for (int i = 0; i < n - 1; i++)
+            {
+                for (int j = 0; j < n - i - 1; j++)
+                {
+                    if (arr[j] > arr[j + 1])
+                    {
+
+                        int temp = arr[j];
+                        arr[j] = arr[j + 1];
+                        arr[j + 1] = temp;
+                    }
+                }
+            }
+        }
+
+        public void displayArray(int[] arr)
+        {
+            foreach (int i in arr)
+            {
+                Console.Write("\t {0}", i);
+            }
+            Console.WriteLine();
+        }
+
+    }
+}
