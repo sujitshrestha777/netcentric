64d9463fde5067a0a0dad629e56dbcf2af865fa2
sujit shrestha <sujitshrestha2060@gamil.com>
2024-12-17 19:32:35 +0545

    mvc sample

---
+++ MVC_Sample/MVC_Sample/Controllers/CategoryController.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MVCeStore.Models;
+
+namespace MVC_Sample.Controllers
+{
+    public class CategoryController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult list()
+        {
+            List<Category> products = new List<Category>
+            {
+                new Category
+                {
+                   Id = 101,
+                   Name = "Table",
+                   Description = "Table is a wooden furniture.",
+                   Price = 15000,
+
+                },
+                new Category
+                {
+                  Id = 205,
+                   Name = "Headphones",
+                    Description = "Headphones are for listening to audio.",
+                        Price = 3000
+
+                },
+                new Category
+                {
+                    Id = 3,
+                    Name = "Watch",
+                    Description = "Watch is a electronic wearable devices.",
+                    Price = 5000
+                }
+            };
+            ViewData["Title"] = products;
+            return View(products);
+        }
+    }
+}
---
+++ MVC_Sample/MVC_Sample/Models/Category.cs
@@ -0,0 +1,11 @@
+﻿namespace MVCeStore.Models
+{
+    public class Category
+    {
+        public int Id { get; set; }
+        public required string Name { get; set; }
+        public required string Description { get; set; }
+        public int Price { get; set; }
+    }
+
+}
